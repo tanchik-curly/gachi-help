1abb11d700f6c306cd3c61ff1bafa20d1c8143b4
Belgef <girnyimykola@gmail.com>
2022-11-07 01:37:50 +0200

    Added basic entities

---
+++ GachiHelp.DAL/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace GachiHelp.DAL.Entities;
+
+public class User : IEntity
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; }
+
+    public string PasswordHash { get; set; }
+}
