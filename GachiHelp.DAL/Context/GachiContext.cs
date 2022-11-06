e9b1e6cd267a2ad2b3b39a3b453e84b4a2bf7ef9
Belgef <girnyimykola@gmail.com>
2022-11-07 01:38:25 +0200

    Added context

---
+++ GachiHelp.DAL/Context/GachiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GachiHelp.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GachiHelp.DAL.Context
+{
+    public class GachiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public GachiContext(DbContextOptions<GachiContext> options) : base(options) { }
+    }
+}
