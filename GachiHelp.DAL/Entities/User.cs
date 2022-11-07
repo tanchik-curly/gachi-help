d9a5ee5149df5531d9df7ef6b2dabae9dfbaa1e9
Belgef <girnyimykola@gmail.com>
2022-11-07 13:05:07 +0200

    Added role to users

--- GachiHelp.DAL/Entities/User.cs
+++ GachiHelp.DAL/Entities/User.cs
@@ -1,10 +1,14 @@
-﻿namespace GachiHelp.DAL.Entities;
+﻿using GachiHelp.DAL.Enums;
+
+namespace GachiHelp.DAL.Entities;
 
 public class User : IEntity
 {
     public int Id { get; set; }
 
     public string Login { get; set; }
+
+    public Role Role { get; set; }
 
     public string PasswordHash { get; set; }
 }
