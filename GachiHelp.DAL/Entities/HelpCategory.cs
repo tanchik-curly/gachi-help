78e437ece663af232d092ca87508dcbbf80580fd
Belgef <girnyimykola@gmail.com>
2022-11-14 01:17:07 +0200

    Added help entities

---
+++ GachiHelp.DAL/Entities/HelpCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace GachiHelp.DAL.Entities
+{
+    public class HelpCategory : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
