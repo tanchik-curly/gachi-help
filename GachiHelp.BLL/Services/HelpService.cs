f66e5ece70356ffcd703c5394c4b74dff9968900
Belgef <girnyimykola@gmail.com>
2022-11-14 11:40:01 +0200

    Added more endpoints to help controller

--- GachiHelp.BLL/Services/HelpService.cs
+++ GachiHelp.BLL/Services/HelpService.cs
@@ -14,11 +14,37 @@ namespace GachiHelp.BLL.Services
             _helpRepository = helpRepository;
         }
 
-        public IEnumerable<Help> GetHelpByUser(int userId, int skip = 0, int limit = -1)
+        public IEnumerable<Help> GetHelp(int skip = 0, int limit = -1)
         {
-            var helps = _helpRepository.FindIncluding(h => h.AuthorId == userId, h => h.HelpCategory!)
+            var helps = _helpRepository.AllIncluding(h => h.Author!, h => h.HelpCategory!)
                 .OrderBy(h => h.CreatedAt).Skip(skip);
             return (limit == -1) ? helps : helps.Take(limit);
+        }
+
+        public IEnumerable<Help> GetHelpByUser(int userId, int skip = 0, int limit = -1)
+        {
+            var helps = _helpRepository.FindIncluding(h => h.AuthorId == userId, h => h.Author!,  h => h.HelpCategory!)
+                .OrderBy(h => h.CreatedAt).Skip(skip);
+            return (limit == -1) ? helps : helps.Take(limit);
+        }
+
+        public IEnumerable<HelpStatAggregateDto> GetHelpStatsByCategory(int? categoryId)
+        {
+            return _helpRepository.FindBy(h => (categoryId == null || h.HelpCategoryId == categoryId))
+                .GroupBy(h => h.Status).Select(g => new HelpStatAggregateDto { Group = g.Key.ToString("G"), Quantity = g.Count() });
+        }
+
+        public IEnumerable<HelpStatAggregateDto> GetHelpStatsByPeriod(DateTime? from, DateTime? to)
+        {
+            DateTime from2 = from ?? _helpRepository.GetAll().Min(u => u.CreatedAt);
+            DateTime to2 = to ?? _helpRepository.GetAll().Max(u => u.CreatedAt);
+
+            return _helpRepository.FindBy(h => h.CreatedAt >= from2 && h.CreatedAt <= to2)
+                .GroupBy(h => h.CreatedAt.ToString("yyyy-MM-01T00:00:00"))
+                .Select(g => new HelpStatAggregateDto { Group = g.Key, Quantity = g.Count() })
+                .UnionBy(Enumerable.Range(0, (to2.Month - from2.Month) + 12 * (to2.Year - from2.Year) + 1)
+                .Select(n => new HelpStatAggregateDto { Group = from2.AddMonths(n).ToString("yyyy-MM-01T00:00:00"), Quantity = 0 }),
+                dto => dto.Group).OrderBy(dto => dto.Group);
         }
 
         public IEnumerable<HelpStatAggregateDto> GetUserHelpStatsByCategory(int userId, int? categoryId)
